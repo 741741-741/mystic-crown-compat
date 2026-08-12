bef1229e127fe4584ffc2a0780398378daede22b
741741 <741741@dev.local>
2026-08-12 11:39:52 +0800

    Add Mixin annotation processor and use @Inject

--- src/main/java/com/_741741/mysticcrowncompat/mixin/LivingEntityMixin.java
+++ src/main/java/com/_741741/mysticcrowncompat/mixin/LivingEntityMixin.java
@@ -1,27 +1,24 @@
 package com._741741.mysticcrowncompat.mixin;
 
-import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(LivingEntity.class)
 public abstract class LivingEntityMixin {
 
-    @ModifyReturnValue(
-        method = "getItemBySlot",
-        at = @At("RETURN")
-    )
-    private ItemStack onGetItemBySlot(ItemStack original, EquipmentSlot slot) {
+    @Inject(method = "getItemBySlot", at = @At("RETURN"), cancellable = true)
+    private void onGetItemBySlot(EquipmentSlot slot, CallbackInfoReturnable<ItemStack> cir) {
         if (slot == EquipmentSlot.HEAD) {
             LivingEntity self = (LivingEntity) (Object) this;
             ItemStack crown = CuriosMixinUtil.getMysticCrownFromCuriosIfPresent(self);
             if (!crown.isEmpty()) {
-                return crown;
+                cir.setReturnValue(crown);
             }
         }
-        return original;
     }
 }
