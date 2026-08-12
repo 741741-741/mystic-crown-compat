717da3003dacc5904d5a411c0b651c5097d4e044
741741 <741741@dev.local>
2026-08-12 16:19:32 +0800

    Add CuriosCompat to register Mystic Crown as ICurio

--- src/main/java/com/_741741/mysticcrowncompat/compat/CuriosCompat.java
+++ src/main/java/com/_741741/mysticcrowncompat/compat/CuriosCompat.java
@@ -1,23 +1,18 @@
 package com._741741.mysticcrowncompat.compat;
 
+import com._741741.mysticcrowncompat.MysticCrownCompat;
 import net.minecraft.sounds.SoundEvents;
-import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.ItemStack;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.capabilities.RegisterCapabilitiesEvent;
-import top.theillusivec4.curios.api.CuriosApi;
 import top.theillusivec4.curios.api.CuriosCapability;
 import top.theillusivec4.curios.api.SlotContext;
-import top.theillusivec4.curios.api.SlotResult;
 import top.theillusivec4.curios.api.type.capability.ICurio;
 import twilightforest.init.TFItems;
 
-import java.util.Optional;
-
-@EventBusSubscriber(modid = "mysticcrowncompat")
+@EventBusSubscriber(modid = MysticCrownCompat.MODID, bus = EventBusSubscriber.Bus.MOD)
 public class CuriosCompat {
-    public static final String SLOT_HEAD = "head";
 
     @SubscribeEvent
     public static void registerCapabilities(RegisterCapabilitiesEvent event) {
@@ -31,12 +26,12 @@ public class CuriosCompat {
 
                 @Override
                 public boolean canEquip(SlotContext slotContext) {
-                    return slotContext.identifier().equals(SLOT_HEAD);
+                    return slotContext.identifier().equals("head");
                 }
 
                 @Override
                 public SoundInfo getEquipSound(SlotContext slotContext) {
-                    return new SoundInfo(SoundEvents.ARMOR_EQUIP_GENERIC.value(), 1.0F, 1.0F);
+                    return new SoundInfo(SoundEvents.ARMOR_EQUIP_GENERIC.value(), 1.0f, 1.0f);
                 }
 
                 @Override
@@ -46,16 +41,5 @@ public class CuriosCompat {
             },
             TFItems.MYSTIC_CROWN.get()
         );
-    }
-
-    public static Optional<ItemStack> findMysticCrownInCurios(LivingEntity entity) {
-        if (!CompatManager.CURIOS_LOADED) {
-            return Optional.empty();
-        }
-        return CuriosApi.getCuriosInventory(entity)
-            .flatMap(handler -> handler.findFirstCurio(
-                stack -> stack.getItem() == TFItems.MYSTIC_CROWN.get()
-            ))
-            .map(SlotResult::stack);
     }
 }
