12379f488de4ab343f5aefbc1c698012387cce85
741741 <741741@dev.local>
2026-08-12 14:24:36 +0800

    Switch to ModifyReturnValue for LivingEntity

---
+++ src/main/java/com/_741741/mysticcrowncompat/mixin/LivingEntityMixin.java
@@ -0,0 +1,24 @@
+package com._741741.mysticcrowncompat.mixin;
+
+import com.llamalad7.mixinextras.injector.ModifyReturnValue;
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.item.ItemStack;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+
+@Mixin(LivingEntity.class)
+public class LivingEntityMixin {
+
+    @ModifyReturnValue(method = "getItemBySlot", at = @At("RETURN"))
+    private ItemStack onGetItemBySlot(ItemStack original, EquipmentSlot slot) {
+        if (slot == EquipmentSlot.HEAD) {
+            LivingEntity self = (LivingEntity) (Object) this;
+            ItemStack crown = CuriosMixinUtil.getMysticCrownFromCuriosIfPresent(self);
+            if (!crown.isEmpty()) {
+                return crown;
+            }
+        }
+        return original;
+    }
+}
