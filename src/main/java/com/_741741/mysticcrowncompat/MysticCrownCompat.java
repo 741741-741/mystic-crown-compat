6bf331f4cec1b35a536f7d14c595e78de9b46353
741741 <741741@dev.local>
2026-08-12 20:03:55 +0800

    Revert to standard Curios tag path without custom datapack

--- src/main/java/com/_741741/mysticcrowncompat/MysticCrownCompat.java
+++ src/main/java/com/_741741/mysticcrowncompat/MysticCrownCompat.java
@@ -1,11 +1,12 @@
 package com._741741.mysticcrowncompat;
 
-import com._741741.mysticcrowncompat.event.CommonEventManager;
+import com._741741.mysticcrowncompat.compat.CuriosCompat;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.config.ModConfig;
-import net.neoforged.neoforge.common.NeoForge;
+import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.neoforged.neoforge.capabilities.RegisterCapabilitiesEvent;
 
 @Mod(MysticCrownCompat.MODID)
 public class MysticCrownCompat {
@@ -13,6 +14,11 @@ public class MysticCrownCompat {
 
     public MysticCrownCompat(IEventBus modEventBus, ModContainer modContainer) {
         modContainer.registerConfig(ModConfig.Type.COMMON, MysticCrownCompatConfig.SPEC);
-        CommonEventManager.init(modEventBus, NeoForge.EVENT_BUS);
+        modEventBus.addListener(this::commonSetup);
+        modEventBus.addListener(CuriosCompat::registerCapabilities);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event) {
+        CompatManager.init();
     }
 }
