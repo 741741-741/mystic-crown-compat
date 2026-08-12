4a6cd64739c22ac67cc3e53a41a3eb4cd58f95bc
741741 <741741@dev.local>
2026-08-12 16:32:32 +0800

    Register CuriosCompat in main mod class

--- src/main/java/com/_741741/mysticcrowncompat/MysticCrownCompat.java
+++ src/main/java/com/_741741/mysticcrowncompat/MysticCrownCompat.java
@@ -1,22 +1,22 @@
 package com._741741.mysticcrowncompat;
 
+import com._741741.mysticcrowncompat.compat.CuriosCompat;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.config.ModConfig;
 import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
-import com._741741.mysticcrowncompat.compat.CompatManager;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import net.neoforged.neoforge.capabilities.RegisterCapabilitiesEvent;
 
 @Mod(MysticCrownCompat.MODID)
 public class MysticCrownCompat {
     public static final String MODID = "mysticcrowncompat";
-    public static final Logger LOGGER = LoggerFactory.getLogger(MODID);
 
     public MysticCrownCompat(IEventBus modEventBus, ModContainer modContainer) {
         modContainer.registerConfig(ModConfig.Type.COMMON, MysticCrownCompatConfig.SPEC);
         modEventBus.addListener(this::commonSetup);
+        // 注册 Curios 能力
+        modEventBus.addListener(CuriosCompat::registerCapabilities);
     }
 
     private void commonSetup(final FMLCommonSetupEvent event) {
