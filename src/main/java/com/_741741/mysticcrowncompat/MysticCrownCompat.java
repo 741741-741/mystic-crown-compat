4cb8d0aa93d2e220504a7d4cb835aa31c6254b8c
741741 <741741@dev.local>
2026-08-12 20:36:06 +0800

    Final: Use @Overwrite for TwilightWand and ZombieWand to check Curios, keep standard tag

--- src/main/java/com/_741741/mysticcrowncompat/MysticCrownCompat.java
+++ src/main/java/com/_741741/mysticcrowncompat/MysticCrownCompat.java
@@ -1,13 +1,11 @@
 package com._741741.mysticcrowncompat;
 
 import com._741741.mysticcrowncompat.compat.CuriosCompat;
-import com._741741.mysticcrowncompat.event.CommonEventManager;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.config.ModConfig;
 import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.neoforged.neoforge.common.NeoForge;
 import net.neoforged.neoforge.capabilities.RegisterCapabilitiesEvent;
 
 @Mod(MysticCrownCompat.MODID)
@@ -18,7 +16,6 @@ public class MysticCrownCompat {
         modContainer.registerConfig(ModConfig.Type.COMMON, MysticCrownCompatConfig.SPEC);
         modEventBus.addListener(this::commonSetup);
         modEventBus.addListener(CuriosCompat::registerCapabilities);
-        CommonEventManager.init(modEventBus, NeoForge.EVENT_BUS);
     }
 
     private void commonSetup(final FMLCommonSetupEvent event) {
